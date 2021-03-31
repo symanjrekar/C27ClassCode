19cdce382868586deaf7880b20fb4061885386ab
symanjrekar <symanjrekar@gmail.com>
2021-03-31 19:33:11 +0530

    Added Slingshot.js and associated code chnage in Sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -15,7 +16,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+    
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -35,15 +36,15 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    log6 = new Log(230,180,80, PI/2);
+    slingshot = new Slingshot(bird.body, log6.body);
 
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+ 
     box1.display();
     box2.display();
     ground.display();
@@ -61,4 +62,7 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    log6.display();
+    slingshot.display();
 }
